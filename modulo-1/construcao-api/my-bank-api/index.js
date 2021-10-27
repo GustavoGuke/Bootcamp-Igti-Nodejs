6989ef824cb0e17736ee169deedda5d4c18a932c
Gustavo Silva <gustavo.athenas13@gmail.com>
2021-10-26 22:11:46 -0300

    Api - configurações iniciais

---
+++ modulo-1/construcao-api/my-bank-api/index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import accountsRouter from "./routes/accounts.js"
+import {promises as fs} from "fs"
+
+
+const {readFile, writeFile, appendFile} = fs
+const app = express()
+app.use(express.json())
+
+app.use("/account", accountsRouter)
+
+app.listen(3000, async () => {
+    const initialJson = {
+        nextId: 1,
+        accounts: []
+    }
+    try {
+      
+        await readFile("accounts.json")
+        
+    } catch (error) {
+        await writeFile("account.json",JSON.stringify(initialJson))
+    }
+  
+   console.log("API Started")
+})
